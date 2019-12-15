b61f953da122326f5904d1579bf137569d938bfc
eddie <eddiem9657@gmail.com>
2019-12-15 18:47:58 -0500

    Classes create

---
+++ src/main/java/HashMap.java
@@ -0,0 +1,8 @@
+public interface HashMap {
+    public void set(String key, Integer value);
+    public void delete(String key);
+    public Integer get(String key);
+    public Boolean isEmpty();
+    public Integer size();
+    public Integer bucketSize(String key);
+}
