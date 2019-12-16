0d16b96147293055ae7e3a085bd794ae07ec6a5c
eddie <eddiem9657@gmail.com>
2019-12-15 19:00:17 -0500

    DashaMapTWo

--- src/main/java/DashaMapTwo.java
+++ src/main/java/DashaMapTwo.java
@@ -1,2 +1,24 @@
 public class DashaMapTwo {
+    public void set(String key, String value){
+
+    }
+    public String delete(String key){
+        return null;
+    }
+
+    public String get(String key){
+        return null;
+    }
+
+
+    public boolean isEmpty(){
+        return false;
+    }
+
+
+    public long size(){
+        return 0;
+    }
+
+
 }
